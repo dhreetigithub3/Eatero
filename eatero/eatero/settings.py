7e6ff6679381187116320b68ec2fb5ab2b387d71
Dhreeti Kumar Sahoo <sahoodhreetikumar@gmail.com>
2026-05-13 17:19:08 +0530

    feat: initialize Django project settings and configure Razorpay credentials

--- eatero/eatero/settings.py
+++ eatero/eatero/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*8z(&x2ot^w@rgd2c%jhyje#qjtpcp2%sof0*n(wh%y3d1^xqc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
