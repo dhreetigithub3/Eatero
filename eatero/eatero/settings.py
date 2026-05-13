5c1693d0c2e131e65e167b564059109c9e29c463
Dhreeti Kumar Sahoo <86562768+dhreetigithub3@users.noreply.github.com>
2026-05-13 18:52:17 +0530

    Remove superuser creation code
    Removed superuser creation logic from settings.py.

--- eatero/eatero/settings.py
+++ eatero/eatero/settings.py
@@ -11,19 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-
-if os.environ.get('CREATE_SUPERUSER') == 'True':
-    from django.contrib.auth import get_user_model
-
-    User = get_user_model()
-
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser(
-            'admin',
-            'admin@gmail.com',
-            'admin123'
-        )
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
