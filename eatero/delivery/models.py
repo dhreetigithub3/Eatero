8e5943a0cac1d3b4a0e869b3da0dbe830a3f5165
Dhreeti Kumar Sahoo <86562768+dhreetigithub3@users.noreply.github.com>
2026-05-14 10:30:52 +0530

    Update model fields for Customer and Restaurant

--- eatero/delivery/models.py
+++ eatero/delivery/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    username = models.CharField(max_length = 20)
-    password = models.CharField(max_length = 20)
-    email = models.CharField(max_length = 20)
+    username = models.CharField(max_length = 10)
+    password = models.CharField(max_length = 10)
+    email = models.CharField(max_length = 30)
     mobile = models.CharField(max_length = 10)
     address = models.CharField(max_length = 50)
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
     picture = models.URLField(max_length=1000, default='https://images.venuebookingz.com/22886-1777034898-wm-triple_eight_bar_(9).jpg')
-    cuisine = models.CharField(max_length=250)
+    cuisine = models.CharField(max_length=200)
     rating = models.FloatField()
 
 class Item(models.Model):
-    restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE, related_name = "items")
-    name = models.CharField(max_length = 50)
-    description = models.CharField(max_length = 400)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="items")
+    name = models.CharField(max_length=200)
+    description = models.TextField()
     price = models.FloatField()
     vegeterian = models.BooleanField(default=False)
     picture = models.URLField(max_length=1000, default='https://www.indiafilings.com/learn/wp-content/uploads/2024/08/How-to-Start-Food-Business.jpg')
